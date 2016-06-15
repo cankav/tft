50b45b2f8380a70eb0a6c9c11764f1221e434a36
Can Kavaklıoğlu <eposta@cankavaklioglu.name.tr>
2016-06-16 01:14:42 +0300

    test_random_tucker3_gtp_performance added

---
+++ test_random_tucker3_gtp_performance.m
@@ -0,0 +1,43 @@
+rand('seed',0);
+test_run_data_filename = 'test_random_tucker3_gtp_run_times.mat';
+
+run_times.cardinalities = [];
+run_times.default = [];
+run_times.steiner = [];
+save( 'test_random_tucker3_gtp_run_times.mat', 'run_times' );
+
+for i=1:10
+    clear all;
+    tft_clear();
+
+    max_cardinality = 10;
+
+    i_ind_card = randi(max_cardinality)+1;
+    j_ind_card = randi(max_cardinality)+1;
+    k_ind_card = randi(max_cardinality)+1;
+    p_ind_card = randi(max_cardinality)+1;
+    q_ind_card = randi(max_cardinality)+1;
+    r_ind_card = randi(max_cardinality)+1;
+
+    load( 'test_random_tucker3_gtp_run_times.mat', 'run_times' );
+    run_times.cardinalities(end+1,:) = [i_ind_card j_ind_card k_ind_card p_ind_card q_ind_card r_ind_card];
+    save( 'test_random_tucker3_gtp_run_times.mat', 'run_times' );
+
+    engine_type = 'default';
+    run test_random_tucker3_gtp_performance_helper
+    load( 'test_random_tucker3_gtp_run_times.mat', 'run_times' );
+    eval( ['run_times.' engine_type '(end+1) = ' num2str(run_time) ';']);
+    save( 'test_random_tucker3_gtp_run_times.mat', 'run_times' );
+
+
+    tft_clear();
+    engine_type = 'steiner';
+    run test_random_tucker3_gtp_performance_helper
+    load( 'test_random_tucker3_gtp_run_times.mat', 'run_times' );
+    eval( ['run_times.' engine_type '(end+1) = ' num2str(run_time) ';'] );
+    save( 'test_random_tucker3_gtp_run_times.mat', 'run_times' );
+
+end
+
+run_times.default ./ run_times.steiner
+mean(run_times.default ./ run_times.steiner)
---
+++ test_random_tucker3_gtp_performance_helper.m
@@ -0,0 +1,56 @@
+i_index = Index(i_ind_card);
+j_index = Index(j_ind_card);
+k_index = Index(k_ind_card);
+p_index = Index(p_ind_card);
+q_index = Index(q_ind_card);
+r_index = Index(r_ind_card);
+
+X = Tensor( i_index, j_index, k_index );
+A = Tensor( i_index, p_index);
+B = Tensor( j_index, q_index);
+C = Tensor( k_index, r_index);
+G = Tensor( p_index, q_index, r_index);
+
+X.data = rand( i_index.cardinality, j_index.cardinality, k_index.cardinality );
+A.data = rand( i_index.cardinality, p_index.cardinality );
+B.data = rand( j_index.cardinality, q_index.cardinality );
+C.data = rand( k_index.cardinality, r_index.cardinality );
+G.data = rand( p_index.cardinality, q_index.cardinality, r_index.cardinality );
+
+pre_process();
+
+p = [1];
+phi = [1];
+
+factorization_model = {X, {A, B, C, G}};
+
+model = TFModel(factorization_model, p, phi);
+
+config = TFEngineConfig(model, 10);
+
+if strcmp(engine_type, 'default')
+    engine = TFDefaultEngine(config, 'gtp_mex');
+elseif strcmp(engine_type, 'steiner')
+    engine = TFSteinerEngine(config, ...
+    [ ...
+        1, -1, 1, -1, -1, 1, -1, -1, ...
+        2, -1, 2, -1, -1, 2, -1, -1, ...
+        3, -1, 3, -1, -1, 3, -1, -1 ...
+        4, -1, 4, -1, -1, 4, -1, -1 ...
+    ],'gtp_mex');
+else
+    error('unknown engine type')
+end
+
+gtp_rules = model.update_rules();
+rule_ind = 1;
+display_rule( gtp_rules{rule_ind}, rule_ind, 'Testing ' );
+input_tensors = gtp_rules{rule_ind}{3};
+
+tic
+gtp_mex(16, gtp_rules{rule_ind}{2}, input_tensors{:} );
+run_time=toc;
+
+%display(['run_time ' engine_type ' ' num2str(run_time)])
+
+check_divergence(engine.kl_divergence);
