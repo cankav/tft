83e2e6a85afc081d8e81af09ac23388cb7f41b8f
Can Kavaklioglu <can.kavaklioglu@ryerson.ca>
2018-12-21 13:36:55 -0500

    added clearing for Index objects in base workspace

--- utils/tft_clear.m
+++ utils/tft_clear.m
@@ -9,3 +9,11 @@ TFT_Index_index = 0;
 
 global TFT_Tensor_index
 TFT_Tensor_index = 0;
+
+% delete all index objects from base workspace
+vars = evalin('base', 'whos');
+for var_ind = 1:length(vars)
+    if strcmp( vars(var_ind).class, 'Index' )
+        evalin('base', ['clear ' vars(var_ind).name ';']);
+    end
+end
